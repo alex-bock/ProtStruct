9695fccdd363a7af56758db255380340d3188e85
Alex Bock <boxkar2@gmail.com>
2023-09-19 16:41:57 -0500

    Revert "UniProt (#2)"
    This reverts commit c87fcff6aac816912ce9ad35a411456cd347ac14.

--- prot_struct/constants.py
+++
@@ -1,7 +0,0 @@
-
-UNIPROT_URL = "https://www.ebi.ac.uk/proteins/api/proteins/"
-
-PDB_URL = "http://files.rcsb.org/download/"
-PDB_EXT = ".pdb"
-
-RCSB_URL = "https://data.rcsb.org/graphql"
--- prot_struct/structure.py
+++ prot_struct/structure.py
@@ -1,20 +1,49 @@
 
 import abc
+import os
+import urllib
 
-from Bio.PDB import Structure
+from Bio.PDB import Structure, PDBParser
 
 
 PDB_EXT = ".pdb"
 PDB_BASE_URL = "http://files.rcsb.org/download/"
 
 
-class ProtStructure(abc.ABC):
+class Structure(abc.ABC):
 
     def __init__(self):
 
         return
-
+    
     @abc.abstractmethod
-    def load_structure(self) -> Structure.Structure:
+    def download_file(self, prot_id: str) -> str:
 
         raise NotImplementedError
+    
+    def load_structure(self, path: str, structure_id: str) -> Structure.Structure:
+
+        structure = self.parser.get_structure(structure_id, path)
+
+        return structure
+    
+
+class PDBStructure(Structure):
+
+    def __init__(self, struct_dir: str = "./data/structures/pdb/"):
+
+        self.parser = PDBParser(QUIET=True)
+        self.structure_dir = struct_dir
+
+        return
+    
+    def download_file(self, prot_id: str) -> str:
+        
+        pdb_file = prot_id + PDB_EXT
+        pdb_url = os.path.join(PDB_BASE_URL, pdb_file)
+        pdb_path = os.path.join(self.structure_dir, pdb_file)
+
+        if not os.path.exists(pdb_path):
+            urllib.request.urlretrieve(pdb_url, pdb_path)
+
+        return pdb_path
